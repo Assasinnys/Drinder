1e370816414e1fbc19980b4bac05e7d2a82bad8e
Assasinnys <dmitry.zenevich@yandex.by>
2020-09-04 19:33:01 +0300

    fix: delete unused methods

--- app/src/main/java/by/hackathon/drinder/util/Extentions.kt
+++ app/src/main/java/by/hackathon/drinder/util/Extentions.kt
@@ -10,15 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.AndroidViewModel
 import by.hackathon.drinder.App
 import by.hackathon.drinder.R
-import by.hackathon.drinder.ui.MainActivity
-import by.hackathon.drinder.ui.MyProfileActivity
 import com.google.android.material.textfield.TextInputLayout
-
-fun Fragment.mainActivity(): MainActivity = (requireActivity() as MainActivity)
-
-fun Fragment.myProfileActivity(): MyProfileActivity = (requireActivity() as MyProfileActivity)
-
-fun Fragment.myApp(): App = (requireActivity().application as App)
 
 fun Context.isLocationPermissionGranted(): Boolean =
     ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
